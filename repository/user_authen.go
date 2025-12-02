7053ded32750d3c4eb34415f17fb203affac405d
Icemodel <Icemodel-@outlook.com>
2025-12-02 15:48:44 +0700

    Finished login user_authen repo

---
+++ repository/user_authen.go
@@ -0,0 +1,36 @@
+package repository
+
+import (
+	"log/slog"
+
+	"mini-erp-backend/model"
+
+	"gorm.io/gorm"
+)
+
+type UserAuthen interface {
+	Search(db *gorm.DB, username string) (*model.User, error)
+}
+
+type userAuthen struct {
+	logger *slog.Logger
+}
+
+func NewUserLogin(logger *slog.Logger) UserAuthen {
+	return &userAuthen{logger: logger}
+}
+
+func (r *userAuthen) Search(db *gorm.DB, username string) (*model.User, error) {
+	var user model.User
+	if err := db.
+		Table("users").
+		Select("user_id", "username", "first_name", "last_name", "password", "role", "created_at", "updated_at").
+		Where("username = ?", username).
+		First(&user).Error; err != nil {
+		if r.logger != nil {
+			r.logger.Error("query user credentials failed", "username", username, "error", err)
+		}
+		return nil, err
+	}
+	return &user, nil
+}
