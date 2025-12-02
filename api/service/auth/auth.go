9927474d56bb61c38f4af27ba907022def302601
Icemodel <Icemodel-@outlook.com>
2025-12-02 15:48:44 +0700

    Setup auth.go bootstrap

--- api/service/auth/auth.go
+++ api/service/auth/auth.go
@@ -2,15 +2,29 @@ package auth
 
 import (
 	"log/slog"
+	"mini-erp-backend/api/service/auth/query"
+	"mini-erp-backend/lib/jwt"
+	"mini-erp-backend/repository"
 
+	"github.com/mehdihadeli/go-mediatr"
 	"gorm.io/gorm"
 )
 
-func NewService(logger *slog.Logger, db *gorm.DB) {
-	// loginService := command.NewLogin(logger, db, userRepo, tokenRepo)
+func NewService(
+	domainDb *gorm.DB,
+	logger *slog.Logger,
+	jwtManager jwt.Manager,
+	userRepo repository.UserAuthen,
+) {
+	LoginService := query.NewLoginByUsername(
+		domainDb,
+		logger,
+		jwtManager,
+		userRepo,
+	)
 
-	// err := mediatr.RegisterRequestHandler(loginService)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	err := mediatr.RegisterRequestHandler(LoginService)
+	if err != nil {
+		panic(err)
+	}
 }
