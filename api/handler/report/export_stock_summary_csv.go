089ec740caa81b9a962818fdd265493a9dc66e17
MeeMeChu <tanakeet005@gmail.com>
2025-12-02 15:39:17 +0700

    🐛 FIX: Swagger

--- api/handler/report/export_stock_summary_csv.go
+++ api/handler/report/export_stock_summary_csv.go
@@ -14,9 +14,9 @@ import (
 //	@Description	Export current stock summary to CSV file
 //	@Tags			Report
 //	@Produce		text/csv
-//	@Success		200	{file}	file
-//	@Failure		500	{object}	api.ErrorResponse
-//	@Router			/reports/stock-summary/export [get]
+//	@Success		200	{file}		file
+//	@Failure		500	{object}	fiber.Map
+//	@Router			/api/v1/reports/stock-summary/export [get]
 func ExportStockSummaryCSV(logger *slog.Logger) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		req := &reportCommand.ExportStockSummaryCSVRequest{}
