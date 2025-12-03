fb509ff74e835d400f03d40afd2f2a6f29b33b8c
Nine <rbxninez@gmail.com>
2025-12-03 17:22:47 +0700

    feat: Implement CRUD operations for purchase order items
    - Added endpoint to list all purchase order items.
    - Updated routes to reflect new endpoint structure.
    - Refactored delete and update operations to remove dependency on purchase order ID.
    - Created new query handler for retrieving all purchase order items.
    - Updated Swagger documentation to include new endpoints and request/response schemas.

---
+++ api/service/purchase_order_item/query/all_purchase_order_items.go
@@ -0,0 +1,38 @@
+package query
+
+import (
+	"context"
+	"log/slog"
+	"mini-erp-backend/api/repository"
+
+	"gorm.io/gorm"
+)
+
+type AllPurchaseOrderItems struct {
+	logger   *slog.Logger
+	db       *gorm.DB
+	ItemRepo repository.PurchaseOrderItem
+}
+
+type AllPurchaseOrderItemsRequest struct{}
+
+func NewAllPurchaseOrderItems(
+	logger *slog.Logger,
+	db *gorm.DB,
+	itemRepo repository.PurchaseOrderItem,
+) *AllPurchaseOrderItems {
+	return &AllPurchaseOrderItems{
+		logger:   logger,
+		db:       db,
+		ItemRepo: itemRepo,
+	}
+}
+
+func (h *AllPurchaseOrderItems) Handle(ctx context.Context, req *AllPurchaseOrderItemsRequest) (interface{}, error) {
+	items, err := h.ItemRepo.Searches(h.db, map[string]interface{}{}, "created_at DESC")
+	if err != nil {
+		return nil, err
+	}
+
+	return items, nil
+}
